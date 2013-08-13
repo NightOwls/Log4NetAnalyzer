f34e69221824ef154358fcaada4a41e5aed17f46
Jason Vella <jvella1975@gmail.com>
2013-08-14 01:42:19 +0200

    All log levels with and without exceptions added
    currently generates 100 of each type so 10k in total … easy to make it
    add more

--- LogGenerator/TestGenerator.cs
+++ LogGenerator/TestGenerator.cs
@@ -6,37 +6,62 @@ namespace LogGenerator
 {
     public class TestGenerator
     {
+        #region Enums
+
+            private enum LogLevel
+            {
+                DEBUG
+                , INFO
+                , WARN
+                , ERROR
+                , FATAL
+            }
+
+        #endregion
+
         #region Private Variables 
 
         private readonly ILog log = LogManager.GetLogger(typeof(TestGenerator).FullName);
-
+       
         #endregion 
 
         #region Switches
 
         //INCLUDE/EXCLUDE
-        private const bool ADD_ERROR_LOGS = true;
-        private const bool ADD_ERROR_EX_LOGS = true;
-        private const bool ADD_INFO_LOGS = true;
-        private const bool ADD_INFO_EX_LOGS = true;
         private const bool ADD_DEBUG_LOGS = true;
         private const bool ADD_DEBUG_EX_LOGS = true;
+        private const bool ADD_INFO_LOGS = true;
+        private const bool ADD_INFO_EX_LOGS = true;
+        private const bool ADD_WARN_LOGS = true;
+        private const bool ADD_WARN_EX_LOGS = true;
+        private const bool ADD_ERROR_LOGS = true;
+        private const bool ADD_ERROR_EX_LOGS = true;
+        private const bool ADD_FATAL_LOGS = true;
+        private const bool ADD_FATAL_EX_LOGS = true;
 
         //HOW MANY OF EACH 
-        private const int NUM_ERROR_LOGS = 10000;
-        private const int NUM_ERROR_EX_LOGS = 10000;
-        private const int NUM_INFO_LOGS = 10000;
-        private const int NUM_INFO_EX_LOGS = 10000;
-        private const int NUM_DEBUG_LOGS = 10000;
-        private const int NUM_DEBUG_EX_LOGS = 10000;
+        private const int NUM_DEBUG_LOGS = 1000;
+        private const int NUM_DEBUG_EX_LOGS = 1000;
+        private const int NUM_INFO_LOGS = 1000;
+        private const int NUM_INFO_EX_LOGS = 1000;
+        private const int NUM_WARN_LOGS = 1000;
+        private const int NUM_WARN_EX_LOGS = 1000;
+        private const int NUM_ERROR_LOGS = 1000;
+        private const int NUM_ERROR_EX_LOGS = 1000;
+        private const int NUM_FATAL_LOGS = 1000;
+        private const int NUM_FATAL_EX_LOGS = 1000;
 
         //MESSAGES
-        private const string ERROR_MSG = "the systen has encounteered an unexpected error ... so FIX IT";
-        private const string ERROR_EX_MSG = "null reference error";
-        private const string INFO_MSG = "some info for you sir!";
-        private const string INFO_EX_MSG = "oh noes! Invalid cast bro!";
         private const string DEBUG_MSG = "we may ave e boouug";
         private const string DEBUG_EX_MSG = "we may ave e boouug";
+        private const string INFO_MSG = "some info for you sir!";
+        private const string INFO_EX_MSG = "oh noes! Invalid cast bro!";
+        private const string WARN_MSG = "Warning ... DANGER Will Robinson ... Warning";
+        private const string WARN_EX_MSG = "Warning ... EXCEPTIONAL DANGER Will Robinson ... Warning";
+        private const string ERROR_MSG = "the system has encounteered an unexpected error ... so FIX IT";
+        private const string ERROR_EX_MSG = "null reference error";
+        private const string FATAL_MSG = "THE WEB SERVER IS DOWWWWWWWNNNN";
+        private const string FATAL_EX_MSG = "THE WEB SERVER IS DOWWWWWWWNNNN AND SOMETHING KILLED IT";
 
         #endregion 
 
@@ -45,101 +70,90 @@ namespace LogGenerator
         [Test]
         public void GenerateLogs()
         {
-            GenerateErrorLogs();
-            GenerateErrorExceptionLogs();
-            GenerateInfoLogs();
-            GenerateInfoExceptionLogs();
-            GenerateDebugLogs();
-            GenerateDebugExceptionLogs();
+            WriteLog(LogLevel.DEBUG, NUM_DEBUG_LOGS, DEBUG_MSG);
+            WriteLog(LogLevel.DEBUG, NUM_DEBUG_EX_LOGS, DEBUG_EX_MSG, GenerateInvalidCastException());
+            WriteLog(LogLevel.INFO, NUM_INFO_LOGS, INFO_MSG);
+            WriteLog(LogLevel.INFO, NUM_INFO_EX_LOGS, INFO_EX_MSG, GenerateApplicationException());
+            WriteLog(LogLevel.WARN, NUM_WARN_LOGS, WARN_MSG);
+            WriteLog(LogLevel.WARN, NUM_WARN_EX_LOGS, WARN_EX_MSG, GenerateInvalidCastException());
+            WriteLog(LogLevel.ERROR, NUM_ERROR_LOGS, ERROR_MSG);
+            WriteLog(LogLevel.ERROR, NUM_ERROR_EX_LOGS, ERROR_EX_MSG, GenerateNullReferenceException());
+            WriteLog(LogLevel.FATAL, NUM_FATAL_LOGS, FATAL_MSG);
+            WriteLog(LogLevel.FATAL, NUM_FATAL_EX_LOGS, FATAL_EX_MSG, GenerateNullReferenceException());
         }
 
         #endregion
 
         #region Private Methods 
         
-        private void GenerateErrorLogs()
+        private NullReferenceException GenerateNullReferenceException()
         {
-
-            if (!ADD_ERROR_LOGS) return;
-
-            for (var i = 0; i < NUM_ERROR_LOGS; i++)
-            {
-                log.Error(ERROR_MSG);
-            }
-        }
-        
-        private void GenerateErrorExceptionLogs()
-        {
-            if (!ADD_ERROR_EX_LOGS) return;
-
             object obj = null;
-
-            for (var i = 0; i < NUM_ERROR_EX_LOGS; i++)
+            
+            try
             {
-                try
-                {
-                    obj.ToString();
-                }
-                catch (NullReferenceException nex)
-                {
-                    log.Error(ERROR_EX_MSG, nex);
-                }
+                obj.ToString();
             }
+            catch (NullReferenceException nex)
+            {
+                return nex;
+            }
+            return null;
         }
 
-        private void GenerateInfoLogs()
+        private InvalidCastException GenerateInvalidCastException()
         {
-            if(!ADD_INFO_LOGS) return;
-
-            for (var i = 0; i < NUM_INFO_LOGS; i++)
-            {
-                log.Info(INFO_MSG);
-            }
-        }
-
-        private void GenerateInfoExceptionLogs()
-        {
-            if(!ADD_INFO_EX_LOGS) return;
-
             object str = "x";
 
-            for (var i = 0; i < NUM_INFO_EX_LOGS; i++)
+            try
             {
-                try
-                {
-                    var x = (int)str;
-                }
-                catch (InvalidCastException icex)
-                {
-                    log.Info(INFO_EX_MSG, icex);
-                }
+                var x = (int)str;
             }
+            catch (InvalidCastException icex)
+            {
+                return icex;
+            }
+
+            return null;
         }
 
-        private void GenerateDebugLogs()
+        private ApplicationException GenerateApplicationException()
         {
-            if (!ADD_DEBUG_LOGS) return;
-
-            for (var i = 0; i < NUM_DEBUG_LOGS; i++)
-            {
-                log.Debug(DEBUG_MSG);
-            }
+            return new ApplicationException("application has failed miserably");
         }
 
-        private void GenerateDebugExceptionLogs()
+        private void WriteLog(LogLevel level, int numLogs, string message)
         {
+            WriteLog(level, numLogs, message, null);
+        }
 
-            if (!ADD_DEBUG_EX_LOGS) return;
-
-            for (var i = 0; i < NUM_DEBUG_EX_LOGS; i++)
+        private void WriteLog(LogLevel level, int numLogs, string message, Exception ex)
+        {
+            for(var i = 0; i < numLogs; i++)
             {
-                try
+                switch (level)
                 {
-                    throw new ApplicationException("application has failed miserably");
-                }
-                catch (ApplicationException aex)
-                {
-                    log.Debug(DEBUG_EX_MSG, aex);
+                    case LogLevel.DEBUG:
+                        if (ex == null && ADD_DEBUG_LOGS) log.Debug(message);
+                        if (ex != null && ADD_DEBUG_EX_LOGS) log.Debug(message, ex);
+                        break;
+                    case LogLevel.INFO:
+                        if (ex == null && ADD_INFO_LOGS) log.Info(message);
+                        if (ex != null && ADD_INFO_EX_LOGS) log.Info(message, ex);
+                        break;
+                    case LogLevel.WARN:
+                        if(ex == null && ADD_WARN_LOGS) log.Warn(message);
+                        if(ex!= null && ADD_WARN_EX_LOGS) log.Warn(message, ex);
+                        break;
+                    case LogLevel.ERROR:
+                        if(ex == null && ADD_ERROR_LOGS) log.Error(message);
+                        if(ex!= null && ADD_ERROR_EX_LOGS) log.Error(message, ex);
+                        break;
+                   case LogLevel.FATAL:
+                       if(ex == null && ADD_FATAL_LOGS) log.Fatal(message);
+                        if(ex!= null && ADD_FATAL_EX_LOGS) log.Fatal(message, ex);
+                        break;
+
                 }
             }
         }
