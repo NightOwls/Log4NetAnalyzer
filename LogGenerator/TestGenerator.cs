56b42ea36b95ea2b0747e1fa6a3470070596e8ca
Jason Vella <jvella1975@gmail.com>
2013-08-13 22:55:47 +0200

    flesh out generators more
    still need to make configuration easier
    can probably make the logic more generic too

--- LogGenerator/TestGenerator.cs
+++ LogGenerator/TestGenerator.cs
@@ -14,20 +14,35 @@ namespace LogGenerator
 
         #region Switches
 
-        //NULL REFERENCE EXCEPTION ERROR LOGS
-        private const bool ADD_NULL_REF_LOGS = true;
-        private const int NUM_NULL_REF_LOGS = 100;
-        private const string NULL_REF_MSG = "null reference error";
+        //ERROR LOGS
+        private const bool ADD_ERROR_LOGS = true;
+        private const int NUM_ERROR_LOGS = 10000;
+        private const string ERROR_MSG = "the systen has encounteered an unexpected error ... so FIX IT";
+
+        //ERROR LOGS WITH EXCEPTION
+        private const bool ADD_ERROR_EX_LOGS = true;
+        private const int NUM_ERROR_EX_LOGS = 10000;
+        private const string ERROR_EX_MSG = "null reference error";
 
         //INFO LOGS
         private const bool ADD_INFO_LOGS = true;
-        private const int NUM_INFO_LOGS = 100;
+        private const int NUM_INFO_LOGS = 10000;
         private const string INFO_MSG = "some info for you sir!";
 
-        //INVALID CAST EXCEPTION INFO LOGS 
+        //INFO LOGS WITH EXCEPTION
         private const bool ADD_INFO_EX_LOGS = true;
-        private const int NUM_INFO_EX_LOGS = 100;
-        private const string INFO_EX_MSG = "some info for you sir!";
+        private const int NUM_INFO_EX_LOGS = 10000;
+        private const string INFO_EX_MSG = "oh noes! Invalid cast bro!";
+
+        //DEBUG LOGS 
+        private const bool ADD_DEBUG_LOGS = true;
+        private const int NUM_DEBUG_LOGS = 10000;
+        private const string DEBUG_MSG = "we may ave e boouug";
+
+        //DEBUG LOGS WITH EXCEPTION
+        private const bool ADD_DEBUG_EX_LOGS = true;
+        private const int NUM_DEBUG_EX_LOGS = 10000;
+        private const string DEBUG_EX_MSG = "we may ave e boouug";
 
         #endregion 
 
@@ -36,22 +51,36 @@ namespace LogGenerator
         [Test]
         public void GenerateLogs()
         {
-            GenerateNullReferenceLogs();
+            GenerateErrorLogs();
+            GenerateErrorExceptionLogs();
             GenerateInfoLogs();
             GenerateInfoExceptionLogs();
+            GenerateDebugLogs();
+            GenerateDebugExceptionLogs();
         }
 
         #endregion
 
         #region Private Methods 
-
-        private void GenerateNullReferenceLogs()
+        
+        private void GenerateErrorLogs()
         {
-            if (!ADD_NULL_REF_LOGS) return;
+
+            if (!ADD_ERROR_LOGS) return;
+
+            for (var i = 0; i < NUM_ERROR_LOGS; i++)
+            {
+                log.Error(ERROR_MSG);
+            }
+        }
+        
+        private void GenerateErrorExceptionLogs()
+        {
+            if (!ADD_ERROR_EX_LOGS) return;
 
             object obj = null;
 
-            for (var i = 0; i < NUM_NULL_REF_LOGS; i++)
+            for (var i = 0; i < NUM_ERROR_EX_LOGS; i++)
             {
                 try
                 {
@@ -59,7 +88,7 @@ namespace LogGenerator
                 }
                 catch (NullReferenceException nex)
                 {
-                    log.Error(NULL_REF_MSG, nex);
+                    log.Error(ERROR_EX_MSG, nex);
                 }
             }
         }
@@ -89,6 +118,34 @@ namespace LogGenerator
                 catch (InvalidCastException icex)
                 {
                     log.Info(INFO_EX_MSG, icex);
+                }
+            }
+        }
+
+        private void GenerateDebugLogs()
+        {
+            if (!ADD_DEBUG_LOGS) return;
+
+            for (var i = 0; i < NUM_DEBUG_LOGS; i++)
+            {
+                log.Debug(DEBUG_MSG);
+            }
+        }
+
+        private void GenerateDebugExceptionLogs()
+        {
+
+            if (!ADD_DEBUG_EX_LOGS) return;
+
+            for (var i = 0; i < NUM_DEBUG_EX_LOGS; i++)
+            {
+                try
+                {
+                    throw new ApplicationException("application has failed miserably");
+                }
+                catch (ApplicationException aex)
+                {
+                    log.Debug(DEBUG_EX_MSG, aex);
                 }
             }
         }
