d10e98a30e52b799400cbe653cf057c4d13e90c3
Jason Vella <jvella1975@gmail.com>
2013-08-18 19:38:08 +0200

    Remove sql provider
    Decided to go with appending directly into the system
    Will implement mongo back end

---
+++ Log.Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Log.Domain;
+
+namespace Log.Data
+{
+    public interface IRepository<T, TK> 
+        where T : IEntity
+        where TK : class, T
+    {
+
+    }
+}
